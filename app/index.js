633dd39dd52f9fb70d4f7ba84e61b808495e6f99
aenonsantiago <aejorisantiago@gmail.com>
2023-05-28 02:23:35 +0800

    shift

---
+++ app/index.js
@@ -0,0 +1,13 @@
+import { useState } from 'react';
+import { Stack, useRouter } from 'expo-router';
+import { View, ScrollView, Text, SafeAreaView } from 'react-native';
+
+const Home = () => {
+    return (
+        <View>
+            <Text>Home</Text>
+        </View>
+    )
+}
+
+export default Home;
